018f7fa3b0405b2a5e59788615ef9403e0aac1ce
Grzegorz Tomicki <gtomik@gmail.com>
2020-07-06 13:21:36 +0200

    :package: refactoring

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,12 +1,14 @@
-const button = document.getElementById('background');
+const dataModules = document.querySelectorAll('[data-module]');
 
-button.addEventListener('click', async (event) => {
-  event.preventDefault();
-  try {
-    const moduleName = button.getAttribute('data-module');
-    const background = await import(/* webpackChunkName: "background" */ `./${moduleName}.js`);
-    background.default();
-  } catch (error) {
-    console.log(error);
-  }
-})
+dataModules.forEach(module => {
+  module.addEventListener('click', async (event) => {
+    event.preventDefault();
+    try {
+      const moduleName = module.getAttribute('data-module');
+      const background = await import(/* webpackChunkName: "[request]" */ `./${moduleName}.js`);
+      background.default();
+    } catch (error) {
+      console.error(error);
+    }
+  })
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,43 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = (env, argv) => ({
-  devtool: argv.mode === 'production' ? 'none' : 'source-map',
-  mode: argv.mode === 'production' ? 'production' : 'development',
-  entry: {
-    script: './sources/js/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './docs'),
-    filename: './[name].[hash].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+function prodPlugin(plugin, mode) {
+  return mode === 'development' ? () => { } : plugin;
+}
+
+module.exports = (env, { mode }) => {
+  const inDev = mode === 'development';
+  return {
+    devtool: inDev ? 'eval-source-map' : 'none',
+    entry: {
+      script: './sources/js/index.js',
+    },
+    output: {
+      path: path.resolve(__dirname, './docs'),
+      filename: './[name].[contenthash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      prodPlugin(
+        new CleanWebpackPlugin({
+          verbose: true,
+        }),
+        mode
+      ),
+      new HtmlWebPackPlugin({
+        filename: 'index.html',
+        template: './sources/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      verbose: true,
-    }),
-    new HtmlWebPackPlugin({
-      filename: 'index.html',
-      template: './sources/index.html',
-    }),
-  ],
-});
+  }
+};
