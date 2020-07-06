9dcb7c17a808dd95e407c6163a816bf72cd72ef6
Grzegorz Tomicki <gtomik@gmail.com>
2020-07-06 17:39:32 +0200

    :package: refactoring

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,11 +1,13 @@
 const dataModules = document.querySelectorAll('[data-module]');
 
-dataModules.forEach(module => {
+dataModules.forEach((module) => {
   module.addEventListener('click', async (event) => {
     event.preventDefault();
     try {
       const moduleName = module.getAttribute('data-module');
-      const background = await import(/* webpackChunkName: "[request]" */ `./${moduleName}.js`);
+      const background = await import(
+        /* webpackChunkName: "[request]" */ `./${moduleName}.js`
+      );
       background.default();
     } catch (error) {
       console.error(error);
